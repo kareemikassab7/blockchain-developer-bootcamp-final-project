34c223f326a026cb7ada61b6bc641a8d3c4d21a0
kareemikassab7 <kareemikassab@aucegypt.edu>
2022-01-22 11:54:37 +0200

    Updates

---
+++ precache-manifest.efab9a05feab50de1938acd51bf79695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ebff1b3476225a1192f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.8273338a.chunk.css"
+  },
+  {
+    "revision": "5ebff1b3476225a1192f",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.5ebff1b3.chunk.js"
+  },
+  {
+    "revision": "96e0cb287e6eebb9b6fd",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/1.00d76cb6.chunk.css"
+  },
+  {
+    "revision": "96e0cb287e6eebb9b6fd",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/1.96e0cb28.chunk.js"
+  },
+  {
+    "revision": "2242dfc52fd5369ed931",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.2242dfc5.js"
+  },
+  {
+    "revision": "27d73437b57faf76e2c15598de7cdb0c",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
